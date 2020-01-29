79014cab38080ee37c4a10bde9fc19aea460ca3f
Alexey <rohtuasad@gmail.com>
2020-01-29 22:57:30 +0300

    Initialize Spring application

---
+++ src/test/java/ru/rohtuasad/springjms/SpringJmsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.rohtuasad.springjms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
