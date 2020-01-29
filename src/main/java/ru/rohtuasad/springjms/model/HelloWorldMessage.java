36ea420897c9097d71ea97d771058c3e12a7215e
Alexey <rohtuasad@gmail.com>
2020-01-29 23:15:31 +0300

    Add POJO class for message

---
+++ src/main/java/ru/rohtuasad/springjms/model/HelloWorldMessage.java
@@ -0,0 +1,20 @@
+package ru.rohtuasad.springjms.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class HelloWorldMessage implements Serializable {
+
+    static final long serialVersionUID = -7881667825436853261L;
+    private UUID id;
+    private String message;
+}
